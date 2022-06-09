6f5d151a8a91e66af5e74f4756511bf395fed5b3
Nikhil Hirulkar <97525857+nikhilhir@users.noreply.github.com>
2022-06-10 00:02:56 +0530

    create-react-app

---
+++ bathandbodyworks/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h4>React-Project</h4>
+    </div>
+  );
+}
+
+export default App;
